17caffe9073443f4a3cff6b271171be5787c9ce1
FadirTorcer <eric.lyonford@gmail.com>
2022-09-13 06:08:14 -0500

    Finished studio, extra missions and all!

--- src/org/launchcode/java/demos/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/demos/studios/areaofacircle/Area.java
@@ -7,17 +7,26 @@ public class Area {
 
         Scanner input = new Scanner(System.in);
         System.out.println("This program finds the area of a circle. Please enter the radius: ");
-        if(input.hasNextDouble()) {
-            double radius = input.nextDouble();
-            double area = Circle.getArea(radius);
-            System.out.println("The area of the circle is : " + area);
+        while (!input.hasNextDouble()) {
+            System.out.println("You know, radius must be a number.");
+            System.out.println("Please enter the radius: ");
+            input.nextLine();
         }
-        //double radius = input.nextDouble();
-        //if (radius < 0) {
-        //    System.out.println("You entered a negative number. There are no circles with negative radii.");
-        //} else if () {
+        double radius = input.nextDouble();
+        while (radius < 0) {
+            System.out.println("You entered a negative number. There are no circles with negative radii.");
+            System.out.println("Please enter the radius: ");
+            while (!input.hasNextDouble()) {
+                System.out.println("You know, radius must be a number.");
+                System.out.println("Please enter the radius: ");
+                input.nextLine();
+            }
+            radius = input.nextDouble();
+        }
 
-        //}
+        double area = Circle.getArea(radius);
+        System.out.println("The area of the circle is : " + area);
+
         input.close();
 
 
