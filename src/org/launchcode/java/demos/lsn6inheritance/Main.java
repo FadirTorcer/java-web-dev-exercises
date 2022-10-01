bbd0a9439fc802040b21e5f61c7b11b43d1def1b
FadirTorcer <eric.lyonford@gmail.com>
2022-09-30 23:35:53 -0500

    Finished lecture modifications

---
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -0,0 +1,19 @@
+package org.launchcode.java.demos.lsn6inheritance;
+
+public class Main {
+
+    public static void main (String[] args) {
+        HouseCat oscar = new HouseCat("Oscar", 8);
+        oscar.eat();
+        System.out.println(oscar.isTired());
+
+        HouseCat oliver = new HouseCat("Oscar");
+        System.out.println(oliver.getWeight());
+
+        Cat plainCat = new Cat(8.6);
+        HouseCat cheshireCat = new HouseCat("Cheshire", 12);
+
+        System.out.println(plainCat.noise()); // prints "Meeeeeeooooowww!"
+        System.out.println(cheshireCat.noise()); // prints "Hello, my name is Cheshire!"
+    }
+}
